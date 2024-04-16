f304c2eeeaef9325b3d252df0996b9c8444fa003
Padee Vang <vangpadee11@gmail.com>
2024-04-15 19:48:22 -0500

    displayed pizzas on DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 
+
 function App() {
+
+const [pizzaArray, setPizzaArray] = useState([]);
+
+  const grabPizza = () => {
+    axios.get('/api/pizza').then(response => {
+    console.log('data response', response.data);
+    setPizzaArray(response.data);
+    }).catch(error => {
+      console.log('error in GET', error);
+      alert('something went wrong in GET');
+    });
+  }
+
+  useEffect(() => {
+    grabPizza();
+  }, []);
 
   return (
     <div className='App'>
@@ -13,6 +30,19 @@ function App() {
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
+  <ul>
+      {pizzaArray.map((pizza) => (
+      
+      <li key ={pizza.id}>
+
+        <p>{pizza.name} {pizza.description} {pizza.price} <img src = {pizza.image_path} /> </p>
+      
+      </li>
+
+      ))}
+
+  </ul>
+
     </div>
   );
 }
