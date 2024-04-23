743d58903681ea7d8099c01cb98ef85327ab48f2
Zoe Lindman <zoe.lindman@gmail.com>
2024-04-22 19:05:59 -0500

    added add to order button

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,17 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 
 // Be sure to replace this reducer! 🙂
-const someReducer = (state = [], action) => {
+const orderList = (state = [], action) => {
+  if (action.type === 'ADD_PIZZA') {
+    let newOrderList = action.payload;
+    return [...state, newOrderList];
+  }
   return state;
 }
 
 const store = createStore(
   combineReducers({
-    someReducer, // 👈 Be sure to replace this, too!
+    orderList, 
   }),
   applyMiddleware(logger),
 );
